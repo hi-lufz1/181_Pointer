0e7add0df06b17bcca9835d735e382e2cc393e52
hi-lufz1 <latifufauzi@gmail.com>
2023-05-08 15:31:45 +0700

    13

--- friendclass/friendclass.cpp
+++ friendclass/friendclass.cpp
@@ -15,5 +15,14 @@ class siswa {
 private :
 	int id;
 public :
-	void set
+	void setId(int pId);
+	void displayAll(orang& a);
 };
+
+void siswa::displayAll(orang& a) {
+	cout << id << endl << a.nama;
+}
+
+void siswa::setId(int pId) {
+	id = pId;
+}
