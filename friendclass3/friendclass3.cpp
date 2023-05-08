e9d0c83ae34aac83037ce80ea48e6a5814bf3822
hi-lufz1 <latifufauzi@gmail.com>
2023-05-08 15:55:36 +0700

    17

---
+++ friendclass3/friendclass3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class pelajar;
+class manusia {
+public:
+	void showNilaiPelajar(pelajar&);
+};
+class pelajar {
+private :
+	int nilai;
+	public
+
+};
