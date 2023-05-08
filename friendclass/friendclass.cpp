a11e7d3415379c762ad8c4e73f2148055680c948
hi-lufz1 <latifufauzi@gmail.com>
2023-05-08 15:21:49 +0700

    13

--- friendclass/friendclass.cpp
+++ friendclass/friendclass.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 using namespace std;
+
+class siswa;
+
+class orang {
+private :
+	string nama;
+public:
+	void setNama(string pNama);
+	friend class siswa;
+}; 
+
+class siswa {
+private :
+	int id;
+public :
+	void set
+};
